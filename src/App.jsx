3054904c53b8bab7d1b9851c198e5542a3b630c5
giorgiovh <jorgevonhoroch@gmail.com>
2022-08-19 00:27:45 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState } from 'react';
+import Card from './components/Card';
+
+const cardsWithSources = [
+  {src: "./images/Lola-and-I.png"},
+  {src: "./images/lola-biting-chair.png"}
+]
+
+function App() {
+  const [cards, setCards] = useState(cardsWithSources);
+
+  return (
+    <div className="App">
+      <h1>Lola's Memory Game</h1>
+      <div className="board">
+        {cards.map(card => (
+          <div>
+            <Card />
+            <img src={card.src} alt='' width="500" height="600"/>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
