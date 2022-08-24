848012c5788eb1e07ec49c86f77b98fc27bae4f4
giorgiovh <jorgevonhoroch@gmail.com>
2022-08-23 23:46:45 -0400

    adds checkIfMatch function

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import { useEffect, useState } from 'react';
 import Card from './components/Card';
 
 const cardsWithSources = [
-  {"src": "./images/Lola-and-I.png"},
-  {"src": "./images/lola-biting-chair.png"},
-  {"src": "./images/lola-close-up.png"},
-  {"src": "./images/Lola-in-couch.png"},
-  {"src": "./images/Lola-on-Laptop.png"},
-  {"src": "./images/Lola-on-plants.png"},
+  { "src": "./images/Lola-and-I.png" },
+  { "src": "./images/lola-biting-chair.png" },
+  { "src": "./images/lola-close-up.png" },
+  { "src": "./images/Lola-in-couch.png" },
+  { "src": "./images/Lola-on-Laptop.png" },
+  { "src": "./images/Lola-on-plants.png" },
 ]
 
 function App() {
   const [cards, setCards] = useState([]);
-  
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
   const shuffleCards = () => {
     const shuffledCards = [...cardsWithSources, ...cardsWithSources]
       .sort(() => Math.random() - .5)
-      .map((card) => ({...card, id: Math.random()}));
+      .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards)
   }
@@ -26,14 +28,31 @@ function App() {
     shuffleCards()
   }, [])
 
+  const checkIfMatch = (choiceOne, choiceTwo) => {
+    choiceOne.src === choiceTwo.src ? alert("Match!") : alert("Not a match")
+  }
+
+  const handleClick = (card) => {
+    if (!choiceOne) {
+      setChoiceOne(card);
+    } else if (!choiceTwo) {
+      setChoiceTwo(card)
+    }
+  }
+
+  useEffect(() => {
+    console.log(choiceOne, choiceTwo);
+    if (choiceOne && choiceTwo) {
+      checkIfMatch(choiceOne, choiceTwo)
+    }
+  }, [choiceOne, choiceTwo])
+
   return (
     <div className="App">
       <h1>Lola's Memory Game</h1>
       <div className="board">
         {cards.map(card => (
-          <div>
-            <Card card={card}/>
-          </div>
+          <Card card={card} handleClick={handleClick} key={card.id} />
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default function Card({ card }) {
+export default function Card({ card, handleClick }) {
+
+
   return (
     <div>
       <img className='front' src={card.src} alt='' width="500" height="600"/>
-      <img className='back' src="./images/lola-card-back-side.png" alt='' width="500" height="600"/>
+      <img className='back' src="./images/lola-card-back-side.png" alt='' width="500" height="600" onClick={() => handleClick(card)}/>
     </div>
   )
 }
