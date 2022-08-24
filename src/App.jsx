8d4b978b875ebbeba1eaf12fc08565c605b94cad
giorgiovh <jorgevonhoroch@gmail.com>
2022-08-24 00:35:33 -0400

    adds flipped prop to Card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 import Card from './components/Card';
 
 const cardsWithSources = [
-  { "src": "./images/Lola-and-I.png" },
-  { "src": "./images/lola-biting-chair.png" },
-  { "src": "./images/lola-close-up.png" },
-  { "src": "./images/Lola-in-couch.png" },
-  { "src": "./images/Lola-on-Laptop.png" },
-  { "src": "./images/Lola-on-plants.png" },
+  { "src": "./images/Lola-and-I.png", matched: false },
+  { "src": "./images/lola-biting-chair.png", matched: false },
+  { "src": "./images/lola-close-up.png", matched: false },
+  { "src": "./images/Lola-in-couch.png", matched: false },
+  { "src": "./images/Lola-on-Laptop.png", matched: false },
+  { "src": "./images/Lola-on-plants.png", matched: false },
 ]
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
   }, [])
 
   const checkIfMatch = (choiceOne, choiceTwo) => {
-    choiceOne.src === choiceTwo.src ? alert("Match!") : alert("Not a match")
+    return choiceOne.src === choiceTwo.src
   }
 
   const handleClick = (card) => {
@@ -43,7 +43,13 @@ function App() {
   useEffect(() => {
     console.log(choiceOne, choiceTwo);
     if (choiceOne && choiceTwo) {
-      checkIfMatch(choiceOne, choiceTwo)
+      if (checkIfMatch(choiceOne, choiceTwo)) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            return card.src === choiceOne.src ? { ...card, matched: true } : { ...card }
+          })
+        })
+      }
     }
   }, [choiceOne, choiceTwo])
 
@@ -52,7 +58,7 @@ function App() {
       <h1>Lola's Memory Game</h1>
       <div className="board">
         {cards.map(card => (
-          <Card card={card} handleClick={handleClick} key={card.id} />
+          <Card card={card} handleClick={handleClick} key={card.id} flipped={card === choiceOne || card === choiceTwo || card.matched}/>
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Card({ card, handleClick }) {
+export default function Card({ card, handleClick, flipped }) {
 
 
   return (
-    <div>
+    <div className={flipped ? "flipped" : ""}>
       <img className='front' src={card.src} alt='' width="500" height="600"/>
       <img className='back' src="./images/lola-card-back-side.png" alt='' width="500" height="600" onClick={() => handleClick(card)}/>
     </div>
